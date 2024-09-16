f5197272a8372aad1fe6dafa667bebb10382c97f
nAMELESS <shushagei@gmail.com>
2024-09-16 23:35:14 +0300

    Input changes in the first task

--- HomeWorks/Dz 21.09/Task1/Task_1.21.09/Task_1.21.09.cpp
+++ HomeWorks/Dz 21.09/Task1/Task_1.21.09/Task_1.21.09.cpp
@@ -10,29 +10,35 @@ private:
 public:
 	Human() {};
 	
-	Human(std::string init, std::string gend)
+	Human(std::string init, std::string gend):
+		initials{init},gender{gend}
 	{
 		initials = init;
 		gender = gend;
 	}
 
-	void InputHuman()
-	{
-		std::cout << "Enter initials of human: ";
-		std::cin >> initials;
-		std::cout << "Enter gender of human: ";
-		std::cin >> gender;
-	}
+	void Display() {std::cout << "Initials: " << initials << " Gender: " << gender;}
 };
 
 class Apartament
 {
 private:
-	std::vector<Human> apartament;
+	std::vector<Human> human;
 	int NumberApartament;
 	int floor;
-public:
 
+public:
+	Apartament(){}
+	
+	Apartament(std::vector<Human> Human, int Num, int floo) :
+		human{ Human }, NumberApartament{ Num }, floor{ floo }
+	{
+		human = Human;
+		NumberApartament = Num;
+		floor = floo;
+	}
+
+	void AddApartaments()
 };
 
 class House
@@ -42,12 +48,41 @@ private:
 	int NumberOfHouse;
 
 public:
+	House(){}
 
+	House(std::vector<Apartament> aparts, int NumberHouse) :
+		house{ aparts },NumberOfHouse(NumberHouse)
+	{
+		house = aparts;
+		NumberOfHouse = NumberHouse;
+	}
+
+	void InputHouse(std::vector<House>&house)
+	{
+		House houseK;
+		std::cout << "Enter number of house: ";
+		std::cin >> houseK.NumberOfHouse;
+		house.push_back(houseK);
+	}
+
+	void PrintVectorOfHouse(std::vector<House>& house)
+	{
+		
+	}
 };
 
 
 int main()
 {
+	std::vector<Human> human;
+	std::vector<Apartament> aprt_s;
 	std::vector<House> house;
+	Human human_s;
+	human_s.InputHuman(human);
+	Apartament apartament;
+	apartament.InputApartament(aprt_s);
+	House house_s;
+	house_s.InputHouse(house);
+	
 	return 0;
 }
