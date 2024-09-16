b50af58d0a62f47abb1fdc58e23e861e190d8acd
nAMELESS <shushagei@gmail.com>
2024-09-16 20:45:28 +0300

    Добавио новое задание

---
+++ HomeWorks/Dz 21.09/Task1/Task_1.21.09/Task_1.21.09.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+class Human
+{
+private:
+	std::string initials;
+	std::string gender;
+public:
+	Human() {};
+	
+	Human(std::string init, std::string gend)
+	{
+		initials = init;
+		gender = gend;
+	}
+
+	void InputHuman()
+	{
+		std::cout << "Enter initials of human: ";
+		std::cin >> initials;
+		std::cout << "Enter gender of human: ";
+		std::cin >> gender;
+	}
+};
+
+class Apartament
+{
+private:
+	std::vector<Human> apartament;
+	int NumberApartament;
+	int floor;
+public:
+
+};
+
+class House
+{
+private:
+	std::vector<Apartament> house;
+	int NumberOfHouse;
+
+public:
+
+};
+
+
+int main()
+{
+	std::vector<House> house;
+	return 0;
+}
