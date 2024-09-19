3da5007e1c52562e74e124bc899dc99d328e7cb6
nAMELESS <shushagei@gmail.com>
2024-09-19 20:39:34 +0300

    Made changes in the second task

--- HomeWorks/Dz 21.09/Task 2/Task 2.21.09/Task 2.21.09.cpp
+++ HomeWorks/Dz 21.09/Task 2/Task 2.21.09/Task 2.21.09.cpp
@@ -2,6 +2,18 @@
 #include <string>
 #include <vector>
 
+enum Actions
+{
+	CHECK_VOLUME=1,
+	CHECK_SQUARE=2,
+	INPUT_DATA=3,
+	CHECK_RESERVOIR=4,
+	DISTRUB_TYPE=5,
+	SQUARES_SEA=6,
+	SQUARES_POND=7,
+	END=0
+};
+
 class Reservoir
 {
 private:
@@ -24,14 +36,46 @@ public:
 		m_type = type;
 	}
 
-	int CheckVolumeOfReservoir()
+	void CheckVolume(std::vector<Reservoir>& reservoirs)
 	{
-		return m_length * m_weight * m_maxDepth;
+		int length;
+		std::cout << "Enter length: ";
+		std::cin >> length;
+		for (Reservoir& reserv : reservoirs)
+		{
+			if (reserv.m_length == length)
+			{
+				std::cout << "Volume: " << reserv.m_length * reserv.m_weight * reserv.m_maxDepth << "\n";
+			}
+		}
 	}
 
-	int CheckSquareOfReservoir()
+	void CheckSquare(std::vector<Reservoir>& reservoirs)
 	{
-		return m_length * m_weight;
+		int length;
+		std::cout << "Enter length: ";
+		std::cin >> length;
+		for (Reservoir& reserv : reservoirs)
+		{
+			if (reserv.m_length == length)
+			{
+				std::cout << "Square: " << reserv.m_length * reserv.m_weight<< "\n";
+			}
+		}
+	}
+
+	void InputDataInvect(std::vector<Reservoir>& reservoir)
+	{
+		Reservoir object;
+		std::cout << "Enter lehgth: ";
+		std::cin >> object.m_length;
+		std::cout << "Enter weigth: ";
+		std::cin >> object.m_weight;
+		std::cout << "Enter max depth: ";
+		std::cin >> object.m_maxDepth;
+		std::cout << "Enter type (sea or pond): ";
+		std::cin >> object.m_type;
+		reservoir.push_back(object);
 	}
 
 	void CheckReservoirType(std::vector<Reservoir>& reservoir)
@@ -45,44 +89,54 @@ public:
 			else if (reserv.m_type == "pond"){ count_pond++; }
 			else {continue;}
 		}
+		if (count_sea == size_vec) { std::cout << "Quantity type of sea: " << count_sea; }
+		else if (count_pond == size_vec) { std::cout << "Quantity type of pond: " << count_pond; }
+		else
+		{
+			std::cout << "Sea: " << count_sea << "\n";
+			std::cout << "Pond: " << count_pond << "\n";
+		}
 	}
 
 	void DistrubitionTypes(std::vector<Reservoir>& reservoir,
 		std::vector<int>&sea,
 		std::vector<int>&pond)
 	{
-		for (Reservoir& reserv : reservoir)
+		for (const Reservoir& reserv : reservoir)
 		{
-			if (reserv.m_type == "sea"){ sea.push_back(reserv.m_length * reserv.m_weight); }
-			else if (reserv.m_type == "pond"){ pond.push_back(reserv.m_length * reserv.m_weight); }
+			if (reserv.m_type == "sea")
+			{ 
+				int result = reserv.m_length * reserv.m_weight;
+				sea.push_back(result); 
+			}
+			else if (reserv.m_type == "pond")
+			{ 
+				int result = reserv.m_length * reserv.m_weight;
+				pond.push_back(result);
+			}
 		}
+		std::cout << "Distrubition is completed!\n";
 	}
 };
 
-int SearchMaxSquareBySea(std::vector<int>& sea)
+void OutputAllSquaresTheSea(std::vector<int>& sea)
 {
-	int max = sea[0];
-	for (size_t i = 0; i < sea.size(); ++i)
+	std::cout << "{ ";
+	for (int i = 0; i < sea.size(); ++i)
 	{
-		if (sea[i] > max)
-		{
-			max = sea[i];
-		}
+		std::cout << sea[i];
 	}
-	return max;
+	std::cout << " }";
 }
 
-int SearchMaxByPond(std::vector<int>& pond)
+void OutputAllSquaresThePond(std::vector<int>& pond)
 {
-	int max = pond[0];
-	for (size_t i = 0; i < pond.size(); ++i)
+	std::cout << "{ ";
+	for (int i = 0; i < pond.size(); ++i)
 	{
-		if (pond[i] > max)
-		{
-			max = pond[i];
-		}
+		std::cout << pond[i];
 	}
-	return max;
+	std::cout << " }";
 }
 
 int main()
@@ -90,7 +144,41 @@ int main()
 	std::vector<Reservoir> reservoir;
 	std::vector<int> sea;
 	std::vector<int> pond;
-
+	Reservoir reserv;
+	int choise;
+	std::cout << "If you wanted check volume, enter 1\nIf you wanted check square, enter 2\nIf you wanted input data in vector, enter 3\nIf you wanted check reservoir, enter 4\nIf you wanted distrubition type, enter 5\nIf you wanted output all sqaures sea, enter 6\nIf you wanted output all sqaures pond,enter 7\nEND = 0\n\n";
+	do
+	{
+		std::cout << "Enter action: ";
+		std::cin >> choise;
+		switch (choise)
+		{
+		case CHECK_VOLUME:
+			reserv.CheckVolume(reservoir);
+			break;
+		case CHECK_SQUARE:
+			reserv.CheckSquare(reservoir);
+			break;
+		case INPUT_DATA:
+			reserv.InputDataInvect(reservoir);
+			break;
+		case CHECK_RESERVOIR:
+			reserv.CheckReservoirType(reservoir);
+			break;
+		case DISTRUB_TYPE:
+			reserv.DistrubitionTypes(reservoir, sea, pond);
+			break;
+		case SQUARES_SEA:
+			OutputAllSquaresTheSea(sea);
+			break;
+		case SQUARES_POND:
+			OutputAllSquaresThePond(pond);
+			break;
+		default:
+			std::cerr << "Uncorrect action\n";
+		}
+	} while (choise != 0);
+	return 0;
 }
 
 
