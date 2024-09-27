0dcae7dcca4647c5ce44dab7623b1b0553642d7d
nAMELESS <shushagei@gmail.com>
2024-09-27 22:26:17 +0300

    Added two tasks

---
+++ HomeWorks/Dz 3.10/Встреча 6/ConsoleApplication1/Task_2/Task_2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <numeric>
+
+class Complex
+{
+private:
+	int m_a;
+	int m_b;
+public:
+	Complex(){}
+	
+	Complex(int a, int b) :m_a{ a }, m_b{ b }
+	{
+		m_a = a;
+		m_b = b;
+	}
+
+	void Input()
+	{
+		std::cout << "Enter a: ";
+		std::cin >> m_a;
+		std::cout << "Enter b: ";
+		std::cin >> m_b;
+	}
+
+	Complex operator+(const Complex&num) const
+	{
+		return Complex(m_a + num.m_a, m_b + num.m_b);
+	}
+
+	Complex operator-(const Complex& num) const
+	{
+		return Complex(m_a - num.m_a, m_b - num.m_b);
+	}
+
+	Complex operator*(const Complex& num)
+	{
+		return Complex(m_a * num.m_a - m_b * num.m_b, m_a * num.m_b + m_b * num.m_a);
+	}
+
+	Complex operator/(const Complex& num)
+	{
+		int denominator = num.m_a * num.m_a + num.m_b * num.m_b;
+		if (denominator == 0)
+		{
+			std::cerr << "You cannot divide by zero\n";
+		}
+		return Complex((m_a * num.m_a + m_b * num.m_b) / denominator, 
+			(m_b * num.m_a - m_a * num.m_b) / denominator);
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Complex& num)
+	{
+		os << num.m_a;
+		if (num.m_a >= 0) { os << "+"; }
+		os << num.m_b << "*i";
+		return os;
+	}
+
+};
+
+int main()
+{
+	Complex number1, number2;
+	number1.Input();
+	number2.Input();
+
+	Complex Summ = number1 + number2;
+	Complex Subtraction = number1 - number2;
+	Complex Composition = number1 * number2;
+	Complex Division = number1 / number2;
+	
+	std::cout << "Summ: " << Summ<<"\n";
+	std::cout << "Subtraction: " << Subtraction << "\n";
+	std::cout << "Composition: " << Composition << "\n";
+	std::cout << "Division: " << Division;
+
+	return 0;
+}
---
+++ HomeWorks/Dz 3.10/Встреча 6/Task1/Task1/Task1.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <numeric> 
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+
+    void simplify()
+    {
+        int gcd = std::gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+
+        if (denominator < 0)
+        {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+    }
+
+public:
+
+    Fraction(int num = 0, int denom = 1) : numerator(num), denominator(denom)
+    {
+        if (denominator == 0)
+        {
+            std::cout << "Denominator cannot be zero.";
+        }
+        simplify();
+    }
+
+    void input()
+    {
+        std::cout << "Enter numerator: ";
+        std::cin >> numerator;
+        std::cout << "Enter denominator: ";
+        std::cin >> denominator;
+        if (denominator == 0) {
+            std::cout << "Denominator cannot be zero.";
+        }
+        simplify();
+    }
+
+    Fraction operator+(const Fraction& other)
+    {
+        return Fraction(numerator * other.denominator + other.numerator * denominator,
+            denominator * other.denominator);
+    }
+
+    Fraction operator-(const Fraction& other)
+    {
+        return Fraction(numerator * other.denominator - other.numerator * denominator,
+            denominator * other.denominator);
+    }
+
+    Fraction operator*(const Fraction& other)
+    {
+        return Fraction(numerator * other.numerator, denominator * other.denominator);
+    }
+
+    Fraction operator/(const Fraction& other)
+    {
+        if (other.numerator == 0)
+        {
+            std::cout << "You can't divide by zero";
+        }
+        return Fraction(numerator * other.denominator, denominator * other.numerator);
+    }
+
+    void output()
+    {
+        std::cout << numerator << "/" << denominator;
+    }
+};
+
+int main() {
+    Fraction fraction1, fraction2;
+    std::cout << "Enter the first fraction:\n";
+    fraction1.input();
+    std::cout << "Enter the second fraction:\n";
+    fraction2.input();
+
+    Fraction sum = fraction1 + fraction2;
+    Fraction difference = fraction1 - fraction2;
+    Fraction product = fraction1 * fraction2;
+    Fraction quotient = fraction1 / fraction2;
+
+    std::cout << "Sum: ";
+    sum.output();
+    std::cout << "\n";
+    std::cout << "Difference: ";
+    difference.output();
+    std::cout << "\n";
+    std::cout << "Product: ";
+    product.output();
+    std::cout << "\n";
+    std::cout << "Quotient: ";
+    quotient.output();
+
+    return 0;
+}
