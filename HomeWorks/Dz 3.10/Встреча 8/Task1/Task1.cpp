2a15e6c966b3951f210b9c663ad8c3fd12d91937
nAMELESS <shushagei@gmail.com>
2024-09-30 23:47:07 +0300

    Внёс изменения

--- HomeWorks/Dz 3.10/Встреча 8/Task1/Task1.cpp
+++ HomeWorks/Dz 3.10/Встреча 8/Task1/Task1.cpp
@@ -1,1 +1,80 @@
-﻿
+﻿#include <iostream>
+#include <cstring>
+
+class String
+{
+private:
+    char* data;
+public:
+    String() : data(nullptr) {}
+
+    String(const char* str)
+    {
+        if (str)
+        {
+            data = new char[strlen(str) + 1];
+            strcpy_s(data, strlen(str) + 1, str);
+        }
+        else { data = nullptr; }
+    }
+
+    ~String() { delete[] data; }
+
+    String(const String& other)
+    {
+        if (other.data)
+        {
+            data = new char[strlen(other.data) + 1];
+            strcpy_s(data, strlen(other.data) + 1, other.data);
+        }
+        else { data = nullptr; }
+    }
+
+    String& operator=(const String& other)
+    {
+        if (this != &other)
+        {
+            delete[] data;
+            if (other.data)
+            {
+                data = new char[strlen(other.data) + 1];
+                strcpy_s(data, strlen(other.data) + 1, other.data);
+            }
+            else { data = nullptr; }
+        }
+        return *this;
+    }
+
+    String(String& other) : data{ other.data } { other.data = nullptr; }
+
+    String& operator=(String&& other)  
+    {
+        if (this != &other)
+        {
+            delete[] data; 
+            data = other.data; 
+            other.data = nullptr; 
+        }
+        return *this;
+    }
+
+    void print() const 
+    {
+        if (data) {std::cout << data<<"\n"; }
+        else { std::cout << "Empty string."; }
+    }
+};
+
+int main() {
+    
+    String s1{ "My string" };
+    std::cout << "First string: ";
+    s1.print();
+
+    String s2 { std::move(s1) };
+    std::cout << "Second string: ";
+    s2.print();
+
+    return 0;
+}
+
--- HomeWorks/Dz 3.10/Встреча 8/Task2/Task2.cpp
+++ HomeWorks/Dz 3.10/Встреча 8/Task2/Task2.cpp
@@ -1,1 +1,64 @@
-﻿
+﻿#include <iostream>
+
+class Array
+{
+private:
+	size_t m_size;
+	int* m_data;
+public:
+	
+	Array(size_t size) :m_size{ size }, m_data{ new int[size] }
+	{
+		std::cout << "Constructor called for size: " << size<<"\n";
+	}
+	Array(const Array& other) : m_size{ other.m_size }, m_data{ new int[other.m_size] }
+	{
+		for (size_t i{ 0 }; i < other.m_size; i++)
+		{
+			m_data[i] = other.m_data[i];
+		}
+		std::cout << "\nCopy constructor called\n";
+	}
+	Array(Array& other) :m_size{ other.m_size }, m_data{ other.m_data }
+	{
+		other.m_size = 0;
+		other.m_data = nullptr;
+	}
+	Array& operator=(Array& other)
+	{
+		if (this != &other)
+		{
+			delete[] m_data;
+			m_data = other.m_data;
+			other.m_data = nullptr;
+		}
+	}
+	~Array() { delete[] m_data; }
+	
+	void print() const
+	{
+		std::cout << "{ ";
+		for (size_t i{ 0 }; i < m_size; i++)
+		{
+			std::cout << m_data[i] << " ";
+		}
+		std::cout << " }";
+	}
+};
+
+
+int main()
+{
+	size_t size;
+	std::cout << "Input size of  array: ";
+	std::cin >> size;
+	Array arr1(size);;
+	std::cout << "First array: \n";
+	arr1.print();
+
+	Array arr2{ std::move(arr1) };
+	std::cout << "Second Array: \n";
+	arr2.print();
+
+	return 0;
+}
