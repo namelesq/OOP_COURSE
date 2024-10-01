e508b35835f9cb682205aa897da08143e6b6a95f
nAMELESS <shushagei@gmail.com>
2024-10-01 19:42:45 +0300

    Добавил последнее задание

--- HomeWorks/Dz 3.10/Встреча 10/Task1/Task1/Task1.cpp
+++ HomeWorks/Dz 3.10/Встреча 10/Task1/Task1/Task1.cpp
@@ -7,6 +7,12 @@ private:
 	int m_month;
 	int m_year;
 
+	
+	void Validate()
+	{
+		if (m_month < 1 || m_month>12) { std::cout << "\nThere is no such month\n"; }
+		if (m_day < 1 || m_day > DaysInMonth()) { std::cout << "\nThere is no such day\n"; }
+	}
 	bool IsLeapYear()
 	{
 		return(m_year % 4 == 0 || m_year % 100 != 0) || (m_year % 400 == 0);
@@ -19,15 +25,10 @@ private:
 		}
 		return (m_month == 4 || m_month == 6 || m_month == 9 || m_month == 1) ? 30 : 31;
 	}
-	void Validate()
-	{
-		if (m_month < 1 || m_month>12) { std::cerr << "There is no such month\n"; }
-		if (m_day > 1 || m_day > DaysInMonth()) { std::cerr << "There is no such day\n"; }
-	}
 
 public:
 
-	Date(int day=1, int month=1, int year=1):m_day{day},m_month{month},m_year{year}
+	Date(int day = 1, int month = 1, int year = 1) :m_day{ day }, m_month{ month }, m_year{ year }
 	{
 		Validate();
 	}
@@ -45,13 +46,13 @@ public:
 				m_year++;
 			}
 		}
-		Validate(); 
+		Validate();
 		return *this;
 	}
 	Date operator--(int days)
 	{
 		Date temp = *this;
-		for (int i{ 0 }; i < days; i++){temp.operator++();}
+		for (int i{ 0 }; i < days; i++) { temp.operator++(); }
 		return temp;
 	}
 
@@ -59,8 +60,8 @@ public:
 	{
 		return m_day != other.m_day && m_month != other.m_month && m_year != other.m_year;
 	}
-	}
-	bool operator(const Date& other)
+
+	bool operator==(const Date& other)
 	{
 		return m_day == other.m_day && m_month == other.m_month && m_year == other.m_year;
 	}
@@ -78,15 +79,15 @@ public:
 		return m_day < other.m_day;
 	}
 
-	friend std::ostream& operator<<(std::ostream& os, const Date& date) 
+	friend std::ostream& operator<<(std::ostream& os, const Date& date)
 	{
 		os << date.m_day << "/" << date.m_month << "/" << date.m_year;
 		return os;
 	}
-	friend std::istream& operator>>(std::istream& is, Date& date) 
+	friend std::istream& operator>>(std::istream& is, Date& date)
 	{
 		is >> date.m_day >> date.m_month >> date.m_year;
-		date.Validate(); 
+		date.Validate();
 		return is;
 	}
 
@@ -104,16 +105,16 @@ public:
 
 	Date operator+=(int days)
 	{
-		for (int i{ 0 }; i < days; i++){operator++();}
+		for (int i{ 0 }; i < days; i++) { operator++(); }
 		return *this;
 	}
 	Date operator-=(int days)
 	{
-		for (int i{ 0 }; i < days; i++){operator++();}
+		for (int i{ 0 }; i < days; i++) { operator++(); }
 		return *this;
 	}
-	void operator()() { std::cout << "Date: " << *this<<"\n"; }
-};
+	void operator()() { std::cout << "Date: " << *this << "\n"; }
+	};
 
 
 int main()
@@ -123,9 +124,12 @@ int main()
 	std::cin >> date_1;
 	std::cout << "Your date: " << date_1;
 	date_1.operator++();
-	std::cout << "Date after ++: " << date_1<<"\n";
+	std::cout << "\nDate after ++: " << date_1 << "\n";
 	Date date_2{ 19,3,2008 };
-	std::cout << "Comparing dates: " << date_1.operator==(date_2)<<"\n";
+	std::cout << "Comparing dates: " << date_1.operator==(date_2) << "\n";
+	date_1.operator=(date_2);
+	std::cout << "The second date is copied to the first date\n";
+	std::cout << "Date 1: " << date_1 << "\n";
 	Date date_3{ 28,1,2008 };
 	if (date_1 > date_3) { std::cout << "First date is more than third date\n"; }
 	else { std::cout << "First date is less than third date\n"; }
