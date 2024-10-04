4cc0d1a39e09cec0a12ede7d153bd9a76b6b2133
nAMELESS <shushagei@gmail.com>
2024-10-04 20:50:52 +0300

    изменения

---
+++ Practics/practic 4.10/Task1/Task1.4.10/Task1.4.10.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+class Circle
+{
+private:
+	int m_radius;
+	int m_length;
+public:
+	Circle(int radius = 0, int length = 0) :m_radius{ radius }, m_length{ length } {}
+
+	void getRadiusAndLength()
+	{
+		std::cout << "Enter radius: ";
+		std::cin >> m_radius;
+		m_length = m_radius * 2 * 3.14;
+	}
+
+	bool operator==(Circle& other)
+	{
+		if (m_radius == other.m_radius) { return true; }
+		else { return false; }
+	}
+
+	Circle operator>(Circle& other)
+	{
+		if (m_length > other.m_length)
+		{
+			std::cout << "First circle is more than second on : ";
+			return  Circle(m_length - other.m_length);
+		}
+		else
+		{
+			std::cout << "Second Circle is more than first on: ";
+			return Circle(other.m_length - m_length);
+		}
+	}
+};
