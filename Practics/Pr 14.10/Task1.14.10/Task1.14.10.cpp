b0301d7f9560c0f8007b07878e9cd42065e1ccb3
nAMELESS <shushagei@gmail.com>
2024-10-05 14:21:40 +0300

    изменения

---
+++ Practics/Pr 14.10/Task1.14.10/Task1.14.10.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+ 
+void FillArray(std::vector<int> vec)
+{
+	srand(time(NULL));
+	for (int i = 0; i < 15; i++)
+	{
+		vec.push_back(1+ rand % 20);
+	}
+}
+
+int main()
+{
+	std::vector<int> Int;
+}
