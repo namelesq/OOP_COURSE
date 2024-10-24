ad48255c3dfbc58978c798cbfc1a87f7dbcb6966
nAMELESS <shushagei@gmail.com>
2024-10-24 19:34:04 +0300

    изменения

---
+++ HomeWorks/DZ 14.10/Встреча 14/Task2_14vstr/Task2_14vstr.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+
+struct PrintJob 
+{
+    string user;
+    int priority;
+    time_t requestTime;
+
+    bool operator<(const PrintJob& other) const 
+    {
+        return priority < other.priority;
+    }
+};
+
+
+struct PrintStat {
+    string user;      
+    time_t printTime; 
+};
+
+
+priority_queue<PrintJob> printQueue;
+
+
+queue<PrintStat> printStats;
+
+
+void addPrintJob(const string& user, int priority) 
+{
+    time_t currentTime = time(nullptr);
+    printQueue.push({ user, priority, currentTime });
+    cout << "Пользователь " << user << " добавил задание на печать с приоритетом " << priority << endl;
+}
+
+
+void processPrintJob() 
+{
+    if (printQueue.empty())
+    {
+        cout << "Нет заданий на печать в очереди." << endl;
+        return;
+    }
+
+    PrintJob job = printQueue.top();
+    printQueue.pop();
+
+    time_t currentTime = time(nullptr);
+    cout << "Печать задания пользователя " << job.user << " с приоритетом " << job.priority << endl;
+
+    printStats.push({ job.user, currentTime });
+}
+
+
+void printStatistics() 
+{
+    if (printStats.empty())
+    {
+        cout << "Статистика печати пуста." << endl;
+        return;
+    }
+
+    cout << "Статистика печати:" << endl;
+    int count = 1;
+    while (!printStats.empty()) 
+    {
+        PrintStat stat = printStats.front();
+        printStats.pop();
+        cout << count << ". Пользователь: " << stat.user
+            << ", Время печати: " << ctime(&stat.printTime);
+        count++;
+    }
+}
+
+int main() {
+    int choice;
+    string user;
+    int priority;
+    setlocale(LC_ALL, "rus");
+    while (true) {
+        cout << "\nМеню:\n";
+        cout << "1. Добавить задание на печать\n";
+        cout << "2. Обработать задание на печать\n";
+        cout << "3. Показать статистику печати\n";
+        cout << "4. Выйти\n";
+        cout << "Введите номер действия: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Введите имя пользователя: ";
+            cin >> user;
+            cout << "Введите приоритет задания (целое число): ";
+            cin >> priority;
+            addPrintJob(user, priority);
+            break;
+        case 2:
+            processPrintJob();
+            break;
+        case 3:
+            printStatistics();
+            break;
+        case 4:
+            cout << "Выход из программы." << endl;
+            return 0;
+        default:
+            cout << "Неверный выбор, попробуйте снова." << endl;
+        }
+    }
+    return 0;
+}
--- HomeWorks/DZ 14.10/Встреча 14/Task_1.14vstr/Task_1.14vstr.cpp
+++ HomeWorks/DZ 14.10/Встреча 14/Task_1.14vstr/Task_1.14vstr.cpp
@@ -1,10 +1,87 @@
 ﻿#include <iostream>
 #include <queue>
-#include <ctime>
+#include <random>
+#include <chrono>
 
 
-
-
-int main()
+double generateExponential(double lambda)
 {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::exponential_distribution<> dis(lambda);
+    return dis(gen);
+}
+
+struct Passenger
+{
+    double arrivalTime;
+};
+struct Bus 
+{
+    double arrivalTime; 
+    int availableSeats; 
+};
+
+int main() {
+
+    double averagePassengerArrivalTime = 5.0; 
+    double averageBusArrivalTime = 15.0;      
+    int maxPeopleOnStop = 10;                 
+    int simulationTime = 480;                 
+    bool isTerminal = false;                  
+
+    std::queue<Passenger> stopQueue;              
+    double currentTime = 0.0;                 
+    double totalWaitingTime = 0.0;            
+    int totalPassengers = 0;                  
+    int peopleLeftDueToFullBus = 0;           
+
+    
+    double passengerArrivalRate = 1.0 / averagePassengerArrivalTime;
+    double busArrivalRate = 1.0 / averageBusArrivalTime;
+
+   
+    double nextPassengerArrival = generateExponential(passengerArrivalRate);
+    double nextBusArrival = generateExponential(busArrivalRate);
+
+    while (currentTime < simulationTime)
+    {
+        
+        if (nextPassengerArrival < nextBusArrival)
+        {
+            currentTime = nextPassengerArrival;
+            if (stopQueue.size() < maxPeopleOnStop) 
+            {
+                stopQueue.push({ currentTime });
+            }
+            
+            nextPassengerArrival = currentTime + generateExponential(passengerArrivalRate);
+        }
+        else 
+        { 
+            currentTime = nextBusArrival;
+            int availableSeats = isTerminal ? 15 : rand() % 16; 
+            int passengersToBoard = std::min(static_cast<int>(stopQueue.size()), availableSeats);
+
+            
+            for (int i = 0; i < passengersToBoard; ++i)
+            {
+                Passenger passenger = stopQueue.front();
+                stopQueue.pop();
+                totalWaitingTime += currentTime - passenger.arrivalTime;
+                totalPassengers++;
+            }
+
+            peopleLeftDueToFullBus += std::max(0, static_cast<int>(stopQueue.size()) - availableSeats);
+
+            nextBusArrival = currentTime + generateExponential(busArrivalRate);
+        }
+    }
+
+    double averageWaitingTime = totalPassengers > 0 ? totalWaitingTime / totalPassengers : 0.0;
+    std::cout << "Average passenger waiting time: " << averageWaitingTime << " minutes." << std::endl;
+    std::cout << "Number of passengers served: " << totalPassengers << std::endl;
+    std::cout << "The number of passengers remaining due to overcrowding: " << peopleLeftDueToFullBus << std::endl;
+
+    return 0;
 }
