31c3794e966927742d80d78cff28f0c83f6af4f3
nAMELESS <shushagei@gmail.com>
2024-11-25 20:51:31 +0300

    Lambda

---
+++ Practics/Lambda-Function/Lambda-Function/LambdaFunc/LambdaFunc.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+class SasNum
+{
+public:
+	int value;
+	SasNum(int value_in) { value = value_in; }
+};
+
+int main()
+{
+	std::vector<SasNum> SasNums = { 8,4,3,3,5,3,4,2,89,12 };
+	std::sort(SasNums.begin(), SasNums.end(), [](const SasNum& L_val, const SasNum& R_val));
+	return 0;
+}
