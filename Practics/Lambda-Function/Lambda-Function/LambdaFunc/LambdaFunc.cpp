542a135812f1de90bb6eb887bb23729ec323711c
nAMELESS <shushagei@gmail.com>
2024-11-26 18:20:20 +0300

    Lambda function

--- Practics/Lambda-Function/Lambda-Function/LambdaFunc/LambdaFunc.cpp
+++ Practics/Lambda-Function/Lambda-Function/LambdaFunc/LambdaFunc.cpp
@@ -11,6 +11,11 @@ public:
 int main()
 {
 	std::vector<SasNum> SasNums = { 8,4,3,3,5,3,4,2,89,12 };
-	std::sort(SasNums.begin(), SasNums.end(), [](const SasNum& L_val, const SasNum& R_val));
+	std::sort(SasNums.begin(), SasNums.end(), [](const SasNum& L_val, const SasNum& R_val)
+		{return L_val.value < R_val.value; });
+	for (int i = 0; i < SasNums.size(); i++)
+	{
+		std::cout << SasNums[i].value<<" ";
+	}
 	return 0;
 }
